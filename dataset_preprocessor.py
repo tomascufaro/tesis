882a718c409337025b9e0835af9a9b3f8e9268ba
Franco <francoj.jofre@gmail.com>
2022-02-08 18:38:08 -0300

    deteccion de outliers

--- dataset_preprocessor.py
+++ dataset_preprocessor.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import random
 from abc import ABC, abstractmethod
 
@@ -116,69 +115,3 @@ class Preprocessor:
         )
 
         return list(ids)
-=======
-import random
-from abc import ABC, abstractmethod
-
-import numpy as np
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.preprocessing import StandardScaler
-from sklearn.utils import shuffle
-
-
-class Scaler(ABC):
-    @abstractmethod
-    def scale(self, x, y):
-        pass
-
-
-class MinMax_Scaler(Scaler):
-    def scale(self, x, y):
-        return MinMaxScaler().fit_transform(x, y)
-
-
-class Standard_Scaler(Scaler):
-    def scale(self, x, y):
-        return StandardScaler().fit_transform(x, y)
-
-
-class Preprocessor:
-    def __init__(self, scaler: Scaler, seed: int, db=None):
-        self.seed = seed
-        self.scaler = scaler
-        self.db = db
-
-    def scale(self, x, y):
-
-        return self.scaler.scale(x, y)
-
-    def downsample(
-        self,
-        x,
-        y,
-        condition,
-        rate,
-    ):
-        random.seed(self.seed)
-        l = np.count_nonzero(np.array(y) == condition)
-        n = int(l * rate)
-        indexes = list(np.where(np.array(y) == condition)[0])
-        indexes = random.sample(indexes, n)
-        x_downsampled = np.delete(np.array(x), indexes, 0)
-        y_downsampled = np.delete(y, indexes, 0)
-
-        return x_downsampled, y_downsampled
-
-    def shuffle(self, x, y):
-        x, y = shuffle(x, y, random_state=self.seed)
-        return x, y
-
-    def delete_features(self):
-        pass
-
-    def get_outliers(self):
-        if self.db == None:
-            raise ("Error: No Database")
-        else:
-            pass
->>>>>>> 7a644d3799ae563dbb17b7b353102d0f86daf149
