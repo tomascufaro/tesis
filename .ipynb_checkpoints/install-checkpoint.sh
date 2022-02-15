bd7de86968d00637b0cbadb49da4b2acea50aca1
root <root@instance-1.asia-east1-a.c.virtual-core-341423.internal>
2022-02-15 18:53:02 +0000

    master wav2vec2

--- .ipynb_checkpoints/install-checkpoint.sh
+++ .ipynb_checkpoints/install-checkpoint.sh
@@ -22,13 +22,6 @@ elif [ -x "$(command -v dnf)" ];     then sudo dnf install $packagesNeeded
 elif [ -x "$(command -v zypper)" ];  then sudo zypper install $packagesNeeded
 else echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install: $packagesNeeded">&2; fi
 
-packagesNeeded='libffi-dev'
-if [ -x "$(command -v apk)" ];       then sudo apk add --no-cache $packagesNeeded
-elif [ -x "$(command -v apt-get)" ]; then sudo apt-get install $packagesNeeded
-elif [ -x "$(command -v dnf)" ];     then sudo dnf install $packagesNeeded
-elif [ -x "$(command -v zypper)" ];  then sudo zypper install $packagesNeeded
-else echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install: $packagesNeeded">&2; fi
-
 pip3 install --upgrade pip3
 pip3 install pymongo[srv]
 pip3 install notebook
